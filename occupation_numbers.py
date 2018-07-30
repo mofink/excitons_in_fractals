36f7c3e1ea08e209f008391a1963a192f247b545
Mo Fink <mosheafink@gmail.com>
2018-07-30 15:49:39 -0400

    added occupation numbers and changed graphs

---
+++ occupation_numbers.py
@@ -0,0 +1,22 @@
+from collections import Counter
+import numpy as np
+
+def occupation_numbers(state_basis,state_num):
+	num_of_times = [0]*len(state_basis)
+	for i in xrange(len(state_basis)):
+		state = state_basis[i]
+		state_dict = Counter(state)
+
+		num_of_times[i] = state_dict.get(state_num , 0)
+
+	return num_of_times
+
+def occupation(mbvects,state_basis,state_num,num_states):
+	mbvects_t = mbvects.transpose()
+	occup = np.zeros(num_states,dtype=complex)
+	B = occupation_numbers(state_basis,state_num)
+	for i in xrange(num_states):
+		occup[i] = int(round(np.dot(mbvects_t[i].conj(),np.multiply(B,mbvects_t[i]))))
+
+	return np.real(occup)	
+
--- quantumA.py
+++ quantumA.py
@@ -11,13 +11,14 @@ from matrix_elements import *
 from vals_vects import *
 from sparse_matrix import *
 from scipy.sparse.linalg import eigsh
+from occupation_numbers import *
 
 
 
 w_max = 13 # Sequence order
 L = 1.0
 W_A = 0.01
-W_B = 0.0188
+W_B = 0.018
 n_max = 100
 W_Fib = gen_Fibonacci_seq(w_max,W_A,W_B)
 M = len(W_Fib)
@@ -29,17 +30,54 @@ V, w = Fib_potential(W_Fib, L, a, eta, N, h)
 phi = np.array([[cmath.exp(1j*(2*pi*n/L)*k*h) for k in xrange(N)] for n in xrange(n_max)], dtype=np.complex)
 vals,vects = get_eigsystem(n_max, V, phi, N, h)
 
-V_0 = 10**(-3)
-Np, Ns = 4, 11
-vmat = new_find_interaction_matrix(vects,V_0,phi,w,Ns,h)
 
+
+##########
+
+
+x = xrange(n_max)
+
+
+EnS = 1.05**2*100/(2*4.5*1.6*9.1)
+aex = 0.8
+
+f, (ax1, ax2) = plt.subplots(1, 2)
+ax1.scatter(x,vals* EnS / (aex*M)**2)
+ax1.set_xlim(-10, 30)
+ax1.set_ylim(0, 5000)
+
+
+
+W_A = 0.01
+W_B = 0.011
+W_Fib = gen_Fibonacci_seq(w_max,W_A,W_B)
+V, w = Fib_potential(W_Fib, L, a, eta, N, h) 
+phi = np.array([[cmath.exp(1j*(2*pi*n/L)*k*h) for k in xrange(N)] for n in xrange(n_max)], dtype=np.complex)
+vals1,vects1 = get_eigsystem(n_max, V, phi, N, h)
+
+
+
+
+ax2.scatter(x,vals1* EnS / (aex*M)**2)
+ax2.set_xlim(-10, 30)
+ax2.set_ylim(0, 5000)
+plt.show()
+
+#######
+
+"""
+
+V_0 = 10**(-3)
+Np, Ns = 4, 12
+vmat = new_find_interaction_matrix(vects,V_0,phi,w,Ns,h)
 
 state_basis = find_states(Np,Ns) #generates state basis
 
 sparse_mtrx = createSparseMatrix(state_basis,vals,vmat,Np)
 sparse_mtrx = sparse_mtrx + sparse_mtrx.getH()
-neigs = 10
+neigs = 10 #number of states
 mbvals,mbvects = eigsh(sparse_mtrx,k=neigs,which='SM')
+
 
 EnS = 1.05**2*100/(2*4.5*1.6*9.1)
 aex = 0.8
@@ -52,6 +90,12 @@ ax1.scatter(x,vals * EnS / (aex*M)**2)
 ax1.set_xlim(0, 100)
 ax2.scatter(mbx, mbvals * EnS / (aex*M)**2)
 plt.show()
+"""
+
+#print occupation(mbvects,state_basis,0,neigs)
+#print occupation(mbvects,state_basis,1,neigs)
+#print occupation(mbvects,state_basis,2,neigs)
+
 
 #print getMatrixElement(i,j,2,state_basis,vals,vmat,N) #i,j are indices, k is max difference to consider,N is number of particles
 
